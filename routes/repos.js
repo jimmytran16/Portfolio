58069d518aa82b240e29269fcf35d4abb889e967
Jimmytran16 <jimmytran1620@gmail.com>
2020-06-18 19:47:38 -0400

    finished updating date sortings

--- routes/repos.js
+++ routes/repos.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const path = require('path');
+const repoUtils  = require('./util/repoUtils.js')
 const axios = require('axios');
 const router = express.Router()
-
+// const endpoint_url = require('./configs').endpoint_url; //Comment out for production
+// console.log(endpoint_url) //Comment out for production
 
 router.get('/',(request,res)=>{ /* Go to the repos HTML page*/
-    const url = process.env.ENDPOINT_URL //get the url endpoint from the enviroment 
+    const url = process.env.ENDPOINT_URL || endpoint_url //get the url endpoint from the enviroment
     axios.get(url)
         .then(response => {
             console.log(typeof(response.data));
             //make a function to get a list of url links to the repos
-            list_of_repos = getListOfRepos(response.data);            
+            list_of_repos = getListOfRepos(response.data);
             res.render(path.join(__dirname,'../views','index.ejs'), {'repos':list_of_repos});
         })
         .catch(error => {
@@ -30,14 +32,15 @@ function getListOfRepos(data){
         else{
             description = d['description']; //if has description, then give var description that value
         }
-        var content = { //objects of content will be passed onto the list 
+        var content = { //objects of content will be passed onto the list
             'fullname':d['full_name'],
             'url':d['html_url'],
-            'update':d['updated_at'],
+            'update':repoUtils.formatDate(d['updated_at'].split("T")[0]),
             'des':description
         };
         list.push(content);
     });
-    return list.reverse();
+    return repoUtils.sortByDate(list);
 }
+
 module.exports = router
---
+++ routes/util/repoUtils.js
@@ -0,0 +1,21 @@
+
+/* function to sort the date in descending order */
+function sortByDate(list){
+    list.sort(function(a, b){ /* This function will sort out the list based on the given dates in ascending order */
+        var aa = a['update'].split('/').reverse().join(),
+            bb = b['update'].split('/').reverse().join();
+        return aa < bb ? -1 : (aa > bb ? 1 : 0);
+    });
+    return list.reverse() /* reverse the list so that it can be in descending order */
+}
+
+function formatDate(date){ /* This function is to format the date into MM/DD/YYYY display */
+    const date_split = date.split("-");
+    return date_split[1] + '/' + date_split[2] + '/' + date_split[0];
+}
+
+
+module.exports = {
+  sortByDate : sortByDate,
+  formatDate : formatDate
+}
